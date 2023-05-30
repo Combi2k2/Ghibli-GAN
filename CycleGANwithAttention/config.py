f580198b6a9f8fed30c5bcc2600ee62394a744e3
Ba Cong Vu <souhhcong@gmail.com>
2023-05-30 18:36:16 +0200

    Tweaking hyper params

--- CycleGANwithAttention/config.py
+++ CycleGANwithAttention/config.py
@@ -10,20 +10,21 @@ LEARNING_RATE = 2e-4  # could also use two lrs, one for gen and one for disc
 BETA = (0.5, 0.999)
 WEIGHT_DECAY = 1e-4
 
-BATCH_SIZE = 8
+BATCH_SIZE = 4
 IMAGE_SIZE = 256
 CHANNELS_IMG = 3
-NUM_EPOCHS = 100
+NUM_EPOCHS = 200
 NUM_WORKERS = 8 if cuda else 0
 
 # model
-FEATURES_DISC = 16
-FEATURES_GEN = 16
+FEATURES_DISC = 64
+FEATURES_GEN = 64
 
-N_DOWNSAMPLING = 2
-N_RESTNET_BLOCK = 1
+N_DOWNSAMPLE_GEN = 2
+N_DOWNSAMPLE_DISC = 3
+N_RESTNET_BLOCK = 3
 
-EMBED_DIM = 120
+EMBED_DIM = 264
 
 # loss design
 ADV_WEIGHT = 1
@@ -37,4 +38,4 @@ LOAD_CHECKPOINT = False
 CHECKPOINT_DIR = 'checkpoints/'
 
 # Dataset
-LABELS = ['City', 'Human', 'Scenery', 'Room']
+LABELS = ['Human']
