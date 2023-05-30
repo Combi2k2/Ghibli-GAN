64ae4fef58dbf2ebc1d60f6f94a0e24ccaff0322
Ba Cong Vu <souhhcong@gmail.com>
2023-05-30 18:35:28 +0200

    Apply Spectral Norm initialization Modify logging frequency

--- CycleGANwithAttention/train.py
+++ CycleGANwithAttention/train.py
@@ -16,7 +16,7 @@ import os
 # Set up dataset
 print("Initializing dataset")
 
-dataset = CartoonDataset('./../data/Cartoon', './../data/Real-Images')
+dataset = CartoonDataset('./../CycleGAN/dataset/photo', './../CycleGAN/dataset/cartoon')
 train_ds, test_ds = train_test_split(dataset, test_size = 0.1)
 
 train_loader = DataLoader(train_ds, batch_size = config.BATCH_SIZE, shuffle = True, num_workers = config.NUM_WORKERS)
@@ -25,6 +25,15 @@ test_loader = DataLoader(test_ds, batch_size = 4, num_workers = config.NUM_WORKE
 # Set up the models
 print("Initializing model")
 trainer = Trainer()
+
+def add_sn(m):
+    if isinstance(m, torch.nn.Conv2d):
+        return torch.nn.utils.spectral_norm(m)
+    else:
+        return m
+
+trainer.discA.apply(add_sn)
+trainer.discB.apply(add_sn)
 
 print(">>  Number of generator's trainable params:", count_parameters(trainer.genA2B))
 print(">>  Number of discriminator's trainable params:", count_parameters(trainer.discA))
@@ -40,15 +49,15 @@ logging.basicConfig(
 )
 logging.captureWarnings(True)
 
-for i in range(config.NUM_EPOCHS):
+for i in range(1, config.NUM_EPOCHS + 1):
     logging.info(f'Epoch [{i}/{config.NUM_EPOCHS}]: Start at {datetime.now()}')
     
     for batch_idx, sample in enumerate(train_loader):
         loss_dict = trainer.run_train(sample)
         loss_history.append(loss_dict)
         
-        if (batch_idx % 1 == 0):
-            loss_string = f"    Batch [{batch_idx + 1} / {len(train_loader)}]:"
+        if (batch_idx % 20 == 0):
+            loss_string = f"    Batch [{batch_idx} / {len(train_loader)}]:"
             loss_string += f" [D_loss: {loss_dict['D_loss']:.4f}]"
             loss_string += f" [G_loss: {loss_dict['G_loss']['total_loss']:.4f} - ("
             loss_string += f"adv: {loss_dict['G_loss']['adv']:.4f}, "
@@ -57,7 +66,7 @@ for i in range(config.NUM_EPOCHS):
             loss_string += f"identity: {loss_dict['G_loss']['identity']:.4f})"
             
             logging.info(loss_string)
-            print(f'Epochs [{i}/{config.NUM_EPOCHS}]', loss_string)
+            print(f'Epochs [{i}/{config.NUM_EPOCHS}]', loss_string[3:])
             
     try:
         test_images = next(test_iter)
