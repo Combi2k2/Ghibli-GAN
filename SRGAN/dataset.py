7e885973caf96970013d4a949240acef424e9283
combi2k2 <cumbee2k2@gmail.com>
2023-07-05 22:17:07 +0200

    fix and test dataset format

--- SRGAN/dataset.py
+++ SRGAN/dataset.py
@@ -1,43 +1,40 @@
-import os
 import numpy as np
 import config
+import os
+
 from torch.utils.data import Dataset, DataLoader
 from PIL import Image
+from os.path import dirname, abspath
 
+
+FILE_DIR = dirname(abspath(__file__))
+REPO_DIR = dirname(FILE_DIR)
 
 class MyImageFolder(Dataset):
     def __init__(self, root_dir):
         super(MyImageFolder, self).__init__()
-        self.data = []
         self.root_dir = root_dir
-        self.class_names = os.listdir(root_dir)
-
-        for index, name in enumerate(self.class_names):
-            files = os.listdir(os.path.join(root_dir, name))
-            self.data += list(zip(files, [index] * len(files)))
+        self.file_list = os.listdir(root_dir)
 
     def __len__(self):
-        return len(self.data)
+        return len(self.file_list)
 
     def __getitem__(self, index):
-        img_file, label = self.data[index]
-        root_and_dir = os.path.join(self.root_dir, self.class_names[label])
-
-        image = np.array(Image.open(os.path.join(root_and_dir, img_file)))
-        image = config.both_transforms(image=image)["image"]
-        high_res = config.highres_transform(image=image)["image"]
-        low_res = config.lowres_transform(image=image)["image"]
+        file_name = self.file_list[index]
+        file_path = os.path.join(self.root_dir, file_name)
+        
+        image = np.array(Image.open(file_path))
+        image = config.both_transforms(image = image)["image"]
+        high_res = config.highres_transform(image = image)["image"]
+        low_res = config.lowres_transform(image = image)["image"]
         return low_res, high_res
 
-
-def test():
-    dataset = MyImageFolder(root_dir="new_data/")
-    loader = DataLoader(dataset, batch_size=1, num_workers=8)
-
-    for low_res, high_res in loader:
-        print(low_res.shape)
-        print(high_res.shape)
-
-
 if __name__ == "__main__":
-    test()
+    dataset = MyImageFolder(root_dir="./../data/Cartoon/Human")
+    
+    print(len(dataset))
+    
+    low_res, high_res = dataset[0]
+    
+    print(low_res.shape)
+    print(high_res.shape)
